62c08bc90db079ec267f16ee7dc60e8941336c39
Airton <airtonluizcanalefilho@hotmail.com>
2024-03-21 17:06:59 -0300

    feat: resultado-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,60 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import 'package:projeto_perguntas/resultado.dart';
+import './questionario.dart';
 
 main() {
   runApp(PerguntaApp());
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
 
-  void _responder() {
-    setState(() {
-      perguntaSelecionada++;
-    });
+  final _perguntas = const [
+    {
+      'texto': 'Qual eh a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual eh o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leao', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual o seu instrutor favorito?',
+      'respostas': [
+        {'texto': 'Leo', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 5},
+        {'texto': 'Joao', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
+    },
+  ];
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _pontuacaoTotal += pontuacao;
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual eh a sua cor favorita?',
-      'Qual eh o seu animal favorito?',
-    ];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -34,14 +68,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
             centerTitle: true,
             backgroundColor: Colors.blue,
           ),
-          body: Column(
-            children: <Widget>[
-              Questao(perguntas[perguntaSelecionada]),
-              Resposta('Resposta 1', _responder),
-              Resposta('Resposta 2', _responder),
-              Resposta('Resposta 3', _responder)
-            ],
-          )),
+          body: temPerguntaSelecionada
+              ? Questionario(_perguntas, _perguntaSelecionada, _responder)
+              : Resultado(_pontuacaoTotal)),
     );
   }
 }
---
+++ lib/resultado.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  const Resultado(this.pontuacao, {super.key});
+
+    String get fraseResultado {
+      if(pontuacao < 8) {
+        return 'Parabens!';
+      }else if(pontuacao < 12){
+        return 'Voce eh bom!';
+      }else if(pontuacao < 16){
+        return 'Impressionante!';
+      }else {
+        return 'Nivel Jedi!';
+      }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(fraseResultado, style: TextStyle(fontSize: 28)),
+    );
+  }
+}
